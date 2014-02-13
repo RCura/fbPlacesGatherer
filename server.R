f1b34d45e4c8d934f8d2481025783a9e7438b666
Robin Cura <robin.cura@gmail.com>
2014-02-13 11:50:08 +0100

    some ideas

--- server.R
+++ server.R
@@ -6,8 +6,8 @@
 #
 
 library(shiny)
-require(httr)
-require(jsonlite)
+require(RCurl)
+require(rjson)
 
 shinyServer(function(input, output) {
   
@@ -37,10 +37,19 @@ LIMIT 100)
 ORDER BY were_here_count DESC
 LIMIT 100'
   token <- "CAACEdEose0cBALbUzGXgMatmZBB6zegBgcCX37zrnLwnBVoMZC2mkSB7c5L8x82y4chdMfDSrS31CFj6O9RQQ6XzZC1hvmXJYr9hZAswWKDj8ku9TTJkb54UHngS648Lt9q8J3nuMyS3ZAYW8RzbEBwU0vHpPGE3j4hJZCfg5SN8w4SxmJJhhYgnm24x2ZCJ8MZD"
-  token2 <- "CAACEdEose0cBANavxSTkTPSVuup2ESHarnji60YO8OxoV2xm7E1bg7d3QFCaf6jOJ0BH2L8ni5c9IRf9g3ASQfKAEDmFB7IZCCZBQ9zZBZBt6nuOzCQZCuLaq32AGgHmaSvRh2v3E7HRYM6MJx0ZBQjsac4oLVL9JHZCAIMbuEeEYjhldcOCgPxh1FAXHClDTUZD"
+  token2 <- "CAACEdEose0cBAKokKISBgliFskhi3rQtnnJ8kIaJFOMOsjwGav3PcvxVonleZB6wZAe73HMmpMqYv3gIPx27UU8E7JAoO5CftgaQFMqEEKKbA0kKBXcE8eJcB3vJM0kAoTteJOYkzOo0ztdtSgp6IKwzwIwum8GBWXXiZCE7sPcG5ZCGQOVUlIwfbvRkdtGZABlohz6R4PgZDZD"
   query <- gsub(" ","+",gsub("\n", "+",query))
   urlQuery <- paste("https://graph.facebook.com/fql?q=",query,"&access_token=", token2)
   
   ### CHECK : https://github.com/tuxette/fbs/blob/master/server.R
   
-})
+  shortQuery <- "SELECT checkins FROM page WHERE page_id = 110522035635343"
+  shortQuery <- gsub(" ","+",shortQuery)
+  shortURLQuery <- paste("https://graph.facebook.com/fql?q=",shortQuery,"&access_token=", token2)
+  GET(shortURLQuery)
+  fromJSON(shortURLQuery)
+  
+  abc <- "https://graph.facebook.com/search?q=coffee&type=place&center=37.76,-122.427&distance=1000&access_token="
+  abcd <- paste(abc, token2, sep="")
+  test <- fromJSON(abcd)
+  })
