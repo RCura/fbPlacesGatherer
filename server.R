d155cfaf37b68a2195e996551623144b352e3545
robin <robin@robin-XPS13>
2014-02-13 18:39:50 +0100

    on the right way

--- server.R
+++ server.R
@@ -8,6 +8,7 @@
 library(shiny)
 require(RCurl)
 require(rjson)
+require(jsonlite)
 
 shinyServer(function(input, output) {
   
@@ -36,20 +37,28 @@ WHERE distance(latitude, longitude, "47.75", "7.33333") < 5000 and NOT is_city
 LIMIT 100)
 ORDER BY were_here_count DESC
 LIMIT 100'
-  token <- "CAACEdEose0cBALbUzGXgMatmZBB6zegBgcCX37zrnLwnBVoMZC2mkSB7c5L8x82y4chdMfDSrS31CFj6O9RQQ6XzZC1hvmXJYr9hZAswWKDj8ku9TTJkb54UHngS648Lt9q8J3nuMyS3ZAYW8RzbEBwU0vHpPGE3j4hJZCfg5SN8w4SxmJJhhYgnm24x2ZCJ8MZD"
-  token2 <- "CAACEdEose0cBAKokKISBgliFskhi3rQtnnJ8kIaJFOMOsjwGav3PcvxVonleZB6wZAe73HMmpMqYv3gIPx27UU8E7JAoO5CftgaQFMqEEKKbA0kKBXcE8eJcB3vJM0kAoTteJOYkzOo0ztdtSgp6IKwzwIwum8GBWXXiZCE7sPcG5ZCGQOVUlIwfbvRkdtGZABlohz6R4PgZDZD"
+  token <- "CAACEdEose0cBALCYk6Nk2XaynTFQDWZBAsQZAqbL0JQWL6ZBBj2RfhLRZAqklt45DskNINnuvSZBSoKyJReVZB3XoYmL1hiiBh7y6FHA6Q5mzzk6wYljiEWwGusqU44heNrlO7fZAlvPp7fI7XVdFwSd53rnc4BIoh9vaZAFR2wlSxR2gdUEpTDIrAvaz8FaMlgZD"
+
   query <- gsub(" ","+",gsub("\n", "+",query))
-  urlQuery <- paste("https://graph.facebook.com/fql?q=",query,"&access_token=", token2)
+  urlQuery <- paste("https://graph.facebook.com/fql?q=",query,"&access_token=", token)
+
   
-  ### CHECK : https://github.com/tuxette/fbs/blob/master/server.R
-  
-  shortQuery <- "SELECT checkins FROM page WHERE page_id = 110522035635343"
-  shortQuery <- gsub(" ","+",shortQuery)
-  shortURLQuery <- paste("https://graph.facebook.com/fql?q=",shortQuery,"&access_token=", token2)
-  GET(shortURLQuery)
-  fromJSON(shortURLQuery)
-  
-  abc <- "https://graph.facebook.com/search?q=coffee&type=place&center=37.76,-122.427&distance=1000&access_token="
-  abcd <- paste(abc, token2, sep="")
+  abc <- "https://graph.facebook.com/search?q=places&type=place&center=48.813333,2.344444&distance=50000&limit=100&fields=id,name,checkins,likes,location,category,category_list&access_token="
+  abcd <- paste(abc, token, sep="")
   test <- fromJSON(abcd)
+  
+  #test <- rbind(test, fromJSON(test$paging$`next`)$data)
+  myDF <- test$data
+  while (!is.null(test$data)){
+    print(nrow(myDF))
+    test <- fromJSON(test$paging$`next`)
+    newData <- test$data
+    print(nrow(newData))
+    currLength <- nrow(myDF)
+    newLength <- nrow(myDF) + nrow(newData)
+    row.names(newData) <- (currLength + 1):(newLength)
+    myDF <- rbind(myDF, newData)
+  }
+  View(cbind(test$data, test$data$location))
+
   })
