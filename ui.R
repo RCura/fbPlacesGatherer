426c3921e96c22cfa1974b3c0294b23fc25b04c2
Robin Cura <robin.cura@gmail.com>
2014-09-30 15:00:29 +0200

    fix2

--- ui.R
+++ ui.R
@@ -16,7 +16,7 @@ shinyUI(pageWithSidebar(
   mainPanel(
     tabsetPanel(
       tabPanel("Plots",
-               chartOutput(outputId="placesMap", lib = 'leaflet'),
+               chartOutput(outputId = "placesMap", lib = 'leaflet'),
                tags$hr(),
                downloadButton('downloadShp', 'Download Shapefile (Lambert 93)'),
                showOutput(outputId="placesStats", lib = "nvd3", )
